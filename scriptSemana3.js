aacf4fd306ea28a4fbaa682378b065476551d0d2
Andres <128700566+KaiqueSandel@users.noreply.github.com>
2024-03-11 15:58:40 -0300

    Update scriptSemana3.js

--- scriptSemana3.js
+++ scriptSemana3.js
@@ -8,9 +8,35 @@ function exercicio1(){
     let year = dateF.getFullYear();
     alert(dayS+", "+day+" de "+month+" de "+year);
 } 
+
 function exercicio2(){
-    alert(setTimeout())
+    function atualizarRelogio() {
+        const atualizando = new Date();
+        const horas = atualizando.getHours().toString().padStart(2, '0');
+        const minutos = atualizando.getMinutes().toString().padStart(2, '0');
+        const segundos = atualizando.getSeconds().toString().padStart(2, '0');
+        
+        const horaAtualizada = `${horas}:${minutos}:${segundos}`;
+
+        document.getElementById('relogio').innerText = horaAtualizada;
+        setTimeout(atualizarRelogio, 1000);
+    }
+    atualizarRelogio();  
 }
 function exercicio3(){
-
+    function verificarPalindromo() {
+        const frase = prompt("Digite uma palavra ou frase:");
+        if (frase === null || frase.trim() === '') {
+            alert("ERROR 3💀! Digite uma frase ou palavra, não o deixe o campo em branco.");
+            return;
+        }
+        const formatar = frase.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+        const verificacaoPalindromo = formatar.split('').reverse().join('');
+        if (formatar === verificacaoPalindromo) {
+            alert(`"A palavra ou frase "${frase}" é um palíndromo!`);
+        } else {
+            alert(`"A palavra ou frase "${frase}" não é um palíndromo.`);
+        }
+    }
+    verificarPalindromo();
 }
